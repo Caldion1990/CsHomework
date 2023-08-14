68dad718cae7cde7f8c0d147aa865c3dd25d176f
Caldion1990 <137450102+Caldion1990@users.noreply.github.com>
2023-08-14 22:32:36 +0300

    Create Task_64.cs

---
+++ Task_64.cs
@@ -0,0 +1,15 @@
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Rec(int number)
+{
+    int i = 1;
+    if (i > number) return i;
+    Console.Write($"{number} ");
+    number--;
+    return Rec(number);
+}
+Rec(number);
